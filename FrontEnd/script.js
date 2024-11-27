aff7fdf739a99e8949644263c3665b8e54fda860
fabien holert <fabienholert@gmail.com>
2024-11-27 21:03:14 +0100

    creation des fonctions d'un filtre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -80,9 +80,23 @@ async function fetchCategories() {
     fetchCategories();
 
 
+// ajout du filtre tous //
+
             let newButton = "Tous"
 
             let button = document.createElement("button");
             button.innerText = newButton;
             let filter = document.getElementById('filtre');
             filtre.appendChild(button);
+
+
+    // Ajout du listener pour filtrer les pièces non abordables
+const boutonTous = document.querySelector("");
+boutonTous.addEventListener("click", function () {
+   const imagesFiltrees = galleryContainer.filter(function (image) {
+       return image.name;
+   });
+   // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+  document.querySelector(".gallery").innerHTML = "";
+  genererImages(boutonTous);
+});

7302cb2f1a4d99c548346e76e6d7d7106f4a189f
fabien holert <fabienholert@gmail.com>
2024-12-01 00:53:19 +0100

    preparation de l ecran de connexion

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -91,3 +91,19 @@ async function fetchCategories() {
 
 
 // creer les filtres fonctionnels//
+
+
+
+// ecran de connexion
+// let form = document.querrySelector(".connexion-main")
+// let baliseEmail = document.getElementById("Email")
+//function verifierChamp(balise) {
+//if(balise.value === "") {
+//  balise.classList.add("error")
+//} else {
+//}
+//} 
+//Form.addEvenListener("connecter", (event) => {
+//    event.preventDefault()
+//  verifierChamp(baliseEmail)
+//})

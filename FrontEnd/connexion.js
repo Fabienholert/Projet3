7deda3afdc2e30dc5311ef4de193b3254de42b7c
fabien holert <fabienholert@gmail.com>
2024-12-05 17:15:52 +0100

    mise a jour visuelle de la page connexion

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,13 +1,14 @@
-// ecran de connexion
-// let form = document.querrySelector(".connexion-main")
-// let baliseEmail = document.getElementById("Email")
-//function verifierChamp(balise) {
-//if(balise.value === "") {
-//  balise.classList.add("error")
-//} else {
-//}
-//} 
-//Form.addEvenListener("connecter", (event) => {
-//    event.preventDefault()
-//  verifierChamp(baliseEmail)
-//})
+//ecran de connexion
+ let form = document.querrySelector('#connexion-main');
+ let baliseEmail = document.getElementById("Email");
+
+ function verifierChamp(balise) {
+if(balise.value === "") {
+balise.classList.add("error")
+} else {
+}
+} 
+Form.addEvenListener("connecter", (event) => {
+    event.preventDefault()
+      verifierChamp(baliseEmail)
+})

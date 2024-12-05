41abbba1da46ee0729863b6c4fd460e336b2b462
fabien holert <fabienholert@gmail.com>
2024-12-05 23:10:19 +0100

    mise a jour de la connexion

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,7 +1,10 @@
 //ecran de connexion
-let contactForm = document.querySelector('connexion-main')
+let contactForm = document.querySelector('.connexion-main input');
 
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
 let fields = document.querySelectorAll ('input')
+fields.forEach((field) =>{resetField});
 let valid = true; 
 fields .forEach((field) =>{
     if(!validateField(field)){
@@ -11,12 +14,24 @@ fields .forEach((field) =>{
 if(valid){
     e.target.submit
 }
+}); false;
 
 
 function validateField(field){
     if(field.checkValidity()){
         return true;
     } else {
+        field.classlist.add('invalid')
+        field.previousElementSibling.insertAdjacentHTML('beforeend', `<span class=msg>${field.validationMessage}</span`)
         return false;
+        }
+        }
+
+function resetField(field){
+    let fieldLabel =  field.previousElementSibling; 
+    field.classlist.remove('invalid')
+    while(fieldLabel.firstElementChild){
+        fieldLabel.removeChild(fieldLabel.firstElementChild);
     }
+    field.valid = true;
 }

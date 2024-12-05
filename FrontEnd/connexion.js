9d843fcc6e2401cd1383954b944be3db9b72dea0
fabien holert <fabienholert@gmail.com>
2024-12-05 22:35:00 +0100

    mise a jour css pour la partie connexion

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,1 +1,22 @@
 //ecran de connexion
+let contactForm = document.querySelector('connexion-main')
+
+let fields = document.querySelectorAll ('input')
+let valid = true; 
+fields .forEach((field) =>{
+    if(!validateField(field)){
+        valid = false;
+    }
+});
+if(valid){
+    e.target.submit
+}
+
+
+function validateField(field){
+    if(field.checkValidity()){
+        return true;
+    } else {
+        return false;
+    }
+}

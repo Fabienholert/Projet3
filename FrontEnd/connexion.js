7b7d0d504b9f2a7b536bbcdbea1aa8649232489e
fabien holert <fabienholert@gmail.com>
2024-12-09 23:24:21 +0100

    mise a jour du fichier connexion

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -1,9 +1,9 @@
 //ecran de connexion
 let contactForm = document.getElementById('logInForm');
-
+  
 async function callLogin (logInBody){
     console.log("appel à /users/login " + JSON.stringify(logInBody))
-}
+};
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -13,28 +13,5 @@ let logInBody = {
     "email": email.value,
     "password": password.value
   };
-
   callLogin (logInBody);
-
-
 });
-
-
-function validateField(field){
-    if(field.checkValidity()){
-        return true;
-    } else {
-        field.classlist.add('invalid')
-        field.previousElementSibling.insertAdjacentHTML('beforeend', `<span class=msg>${field.validationMessage}</span`)
-        return false;
-        }
-        }
-
-function resetField(field){
-    let fieldLabel =  field.previousElementSibling; 
-    field.classlist.remove('invalid')
-    while(fieldLabel.firstElementChild){
-        fieldLabel.removeChild(fieldLabel.firstElementChild);
-    }
-    field.valid = true;
-}

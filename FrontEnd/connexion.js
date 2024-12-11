11bdc95adcbce7b21df5e3e87bb8e3a63f20a098
fabien holert <fabienholert@gmail.com>
2024-12-11 16:40:15 +0100

    ajout de la validation de la connexion + redirection vers index.html

--- FrontEnd/connexion.js
+++ FrontEnd/connexion.js
@@ -14,4 +14,27 @@ let logInBody = {
     "password": password.value
   };
   callLogin (logInBody);
-});
+
+ fetch ("http://localhost:5678/api/users/login", {     
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(logInBody)})
+    .then(response => {
+    if (response.status === 200) {
+        const data = response.json();
+        const {userId, token } = data;
+        window.localStorage.setItem("userId", userId);
+        window.localStorage.setItem("token", token);
+
+        console.log("Connexion réussie !");
+        console.log("userId:", userId, "token:", token);
+
+        window.location.href="/FrontEnd/index.html";
+    } else {
+        alert("Veuillez saisir un Email et Mot de Passe valide !") ;
+    }
+})
+.catch(error => {
+    console.error("Erreur lors de la requête :", error);
+})});
+

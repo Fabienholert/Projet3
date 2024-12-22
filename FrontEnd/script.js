e0ee41c50d0d650d00d8bd4061d593631553d20d
fabien holert <fabienholert@gmail.com>
2024-12-22 21:37:54 +0100

    ajout de la fonction de rajout d'image

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -236,7 +236,7 @@ function checkToken (iat,exp){
             modaleContent.classList.add("modale-content");
             modaleAjout.textContent = "Ajouter une photo";
     
-            // Ajout des images dans la modale
+            // visualisation des images dans la modale
             works.forEach((work) => {
                 const modaleItem = document.createElement("div");
                 modaleItem.classList.add("modale-item");
@@ -306,3 +306,36 @@ function checkToken (iat,exp){
     }
 
     affichageModale();
+
+//deleteButton.addEventListener("click", () => {
+//    if (!work.id) {
+//        console.error('Erreur : ID non défini pour le projet', work);
+//        return;
+//    }
+
+//    const deleteUrl = `http://localhost:5678/api/works/${work.id}`;
+//    const token = window.localStorage.getItem("token"); // Récupérez le token
+
+//    if (!token) {
+//        alert('Vous devez être connecté pour effectuer cette action.');
+//        return;
+//    }
+
+//    fetch(deleteUrl, { 
+//        method: "DELETE",
+//        headers: { 
+//            "Content-Type": "application/json",
+//            "Authorization": `Bearer ${token}` // Ajoutez le token ici
+//        },
+//    })
+//    .then((response) => {
+//        if (!response.ok) {
+//            throw new Error(`Erreur lors de la suppression : ${response.status}`);
+//        }
+//        console.log(`Projet avec ID ${work.id} supprimé`);
+//        modaleItem.remove(); // Supprime l'élément du DOM
+//    })
+//    .catch((error) => {
+//        console.error('Erreur lors de la suppression :', error);
+//    });
+//});

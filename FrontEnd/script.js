425519eb30af21d9e871e5d3c4db8db1340f4f8f
fabien holert <fabienholert@gmail.com>
2024-12-27 15:21:48 +0100

    mise a jour bouton de validation

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -316,6 +316,7 @@ else {
             modaleCategoriePage.textContent="Catégorie";
             inputCategorie.classList.add("inputCategorie");
             modaleValider.classList.add("bouton-Valider");
+            modaleValider.setAttribute('type', 'submit');
             modaleValider.textContent = "Valider";
             modaleContentAjout.classList.add("modaleContentAjout");
             
@@ -388,11 +389,68 @@ else {
                     console.log(inputFile.setAttribute.value)
                 });
 
-               
-              modaleValider.addEventListener("click", () => {
+                function validationButton (inputFile, inputCategorie, modaleValider ){
+                    console.log("tout est ok")
+                    }
 
-        })})})}; 
+
+
+        })})}; 
 
 affichageModale();
+
+
+
+
+
+//let contactForm = document.getElementById('logInForm');
+  
+//async function callLogin (logInBody){
+//    console.log("appel à /users/login " + JSON.stringify(logInBody))
+//};
+
+//contactForm.addEventListener('submit', (e) => {
+//    e.preventDefault();
+//let email = document.getElementById('Email');
+//let password = document.getElementById('motDePasse');
+//let logInBody = {
+//    "email": email.value,
+//    "password": password.value
+//  };
+//  console.log("appel à /users/login " + JSON.stringify(logInBody));
+
+// fetch ("http://localhost:5678/api/users/login", {     
+//    method: "POST",
+//    headers: { "Content-Type": "application/json" },
+//    body: JSON.stringify(logInBody),
+//})
+
+//    .then(response => {
+//    if (response.status === 200) { 
+//        return response.json();
+//        } else {
+//            alert("Veuillez saisir un Email et Mot de Passe valide !");
+//        }
+//    })
+
+//        .then(data => {
+//            console.log("données recues :", data);
+
+//            const {userId, token } = data;
+
+//        window.localStorage.setItem("token", token);
+//        window.localStorage.setItem("userId", userId);
+
+//        console.log("Connexion réussie !");
+//        console.log("token:", token, "userId:", userId);
+//        console.log("Classe active ajoutée au body");
+//        window.location.href="index.html";
+//    })
+    
+//.catch(error => {
+//    console.error("Erreur lors de la requête :", error);
+
+//});
+//});
 
 
